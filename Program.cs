88a895d09f8d146dd99ca8d4640f8ca43694d6d1
Vidget <vidget@yahoo.com>
2015-07-07 16:40:54 -0400

    Convert UpperCase

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_7_15_ExperIT_StringLab_UpperCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int x = 0; x < args.Length; x++)
+            {
+                Console.Write(args[x].ToUpper());
+                Console.Write(" ");
+            }
+
+            Console.ReadLine();
+        }
+   
+
+
+
+        
+    }
+}
